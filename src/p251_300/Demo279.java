674917e61b36c88a6a6e0de143ab257d515f5211
10471 <1047106689@qq.com>
2021-02-28 16:13:20 +0800

    2021/2/28

--- src/p251_300/Demo279.java
+++ src/p251_300/Demo279.java
@@ -25,6 +25,7 @@ public class Demo279 {
 
         int[] dp = new int[n + 1];
         for (int i = 1; i <= n; i++) {
+            // 最多可以分解为 i 个1
             dp[i] = i;
             for (int j = 1; j * j <= i; j++) {
                 dp[i] = Math.min(dp[i], dp[i - j * j]+1);
