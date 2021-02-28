c61a0b68918cb2e91d1cf1fef65da8c6a4965d96
10471 <1047106689@qq.com>
2021-02-28 09:46:45 +0800

    2021/2/28

--- src/p51_100/Demo70.java
+++ src/p51_100/Demo70.java
@@ -8,4 +8,34 @@ package p51_100;
  * @author hc
  */
 public class Demo70 {
+
+    public int climbStairs(int n) {
+
+        /**
+         * 递归
+         */
+        if (n == 1){return 1;}
+        if (n == 2){return 2;}
+//        return climbStairs(n-1)+climbStairs(n-2);
+
+        /**
+         * 类斐波那契额数列 记忆化搜索
+         */
+        int[] memo = new int[n];
+        if (n>2){
+            memo[n] = climbStairs(n-1)+ climbStairs(n-2);
+        }
+
+        /**
+         * 自底向上 动态规划
+         */
+        memo[1] = 1;
+        memo[2] = 2;
+        for (int i = 3; i < n+1; i++) {
+            memo[i] = memo[i-1]+memo[i-2];
+        }
+
+
+        return memo[n];
+    }
 }
