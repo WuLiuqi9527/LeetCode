bd2427db0af8d76d24e3528097f150a01067ce9d
10471 <1047106689@qq.com>
2021-03-27 20:10:55 +0800

    2021/3/27

--- src/DayDemo.java
+++ src/DayDemo.java
@@ -1,54 +1,6 @@
-import p51_100.Demo97;
-
-import java.util.ArrayList;
-
 /**
  * @author hc
  */
 public class DayDemo {
 
-    public static class ListNode {
-        int val;
-        ListNode next;
-
-        ListNode() {
-        }
-
-        ListNode(int val) {
-            this.val = val;
-        }
-
-        ListNode(int val, ListNode next) {
-            this.val = val;
-            this.next = next;
-        }
-    }
-
-    public ListNode rotateRight(ListNode head, int k) {
-
-        if (head == null || head.next == null || k <= 0) {
-            return head;
-        }
-
-        ArrayList<ListNode> mem = new ArrayList<>();
-        ListNode node = head;
-        while (node != null) {
-            mem.add(node);
-            node = node.next;
-        }
-
-        int len = mem.size();
-        for (int i = 0; i < k; i++) {
-            int pre = len - 2;
-            int last = len - 1;
-            mem.get(pre).next = null;
-            mem.get(last).next = head;
-            head = mem.get(last);
-        }
-        return head;
-    }
-
-    public static void main(String[] args) {
-        new DayDemo().rotateRight(new ListNode(0,new ListNode(1,new ListNode(2))), 4);
-    }
 }
--- src/p51_100/Demo61.java
+++ src/p51_100/Demo61.java
@@ -14,7 +14,7 @@ package p51_100;
  */
 public class Demo61 {
 
-    public class ListNode {
+    public static class ListNode {
         int val;
         ListNode next;
 
@@ -33,31 +33,36 @@ public class Demo61 {
 
     public ListNode rotateRight(ListNode head, int k) {
 
-        if (head == null || head.next == null || k == 0) {
+        if (head == null || head.next == null || k <= 0) {
             return head;
         }
 
-        int count = 1;
-        ListNode pre = head;
-        while (pre.next != null) {
-            pre = pre.next;
-            ++count;
+        int len = 1;
+        ListNode last = head;
+        while (last.next != null) {
+            last = last.next;
+            ++len;
         }
 
-        // k > count (链表的长度)
-        k = k % count;
+        // 滤去多余循环
+        k = k % len;
         if (k == 0) {
             return head;
         }
 
-        pre.next = head;
-        for (int i = 0; i < count - k; i++) {
-            pre = pre.next;
+        // 构造环形链表，直接找 last 将要指向的节点，其下一个节点即为头结点
+        last.next = head;
+        // 往前挪 k 位 相等于 往后挪 len-k 位
+        for (int i = 0; i < len - k; i++) {
+            last = last.next;
         }
-
-        ListNode res = pre.next;
-        pre.next = null;
+        ListNode res = last.next;
+        last.next = null;
 
         return res;
+    }
+
+    public static void main(String[] args) {
+        new Demo61().rotateRight(new ListNode(0, new ListNode(1, new ListNode(2))), 4);
     }
 }
