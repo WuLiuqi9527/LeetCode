7a6896e78d078dff0d8e34f6d2d70302c4f67358
10471 <1047106689@qq.com>
2021-03-28 16:18:39 +0800

    2021/3/28

---
+++ src/p151_200/Demo171.java
@@ -0,0 +1,44 @@
+package p151_200;
+
+/**
+ * 给定一个Excel表格中的列名称，返回其相应的列序号。
+ * <p>
+ * 例如，
+ *      A -> 1
+ *      B -> 2
+ *      C -> 3
+ *      ...
+ *      Z -> 26
+ *      AA -> 27
+ *      AB -> 28
+ * ...
+ * 示例：
+ * 输入: "AB"
+ * 输出: 28
+ * <p>
+ * 示例 3:
+ * 输入: "ZY"
+ * 输出: 701
+ *
+ * @author hc
+ */
+public class Demo171 {
+
+    /**
+     * 本质是 26 进制的转化
+     */
+    public int titleToNumber(String columnTitle) {
+
+        int res = 0;
+        for (char ch : columnTitle.toCharArray()){
+            res = res * 26 + ch - 'A' + 1;
+        }
+
+        return res;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new Demo171().titleToNumber("ZY"));
+        System.out.println(new Demo171().titleToNumber("AB"));
+    }
+}
---
+++ src/p151_200/Demo172.java
@@ -0,0 +1,26 @@
+package p151_200;
+
+/**
+ * 给定一个整数 n，返回 n! 结果尾数中零的数量。
+ * <p>
+ * 示例 1:
+ * 输入: 3
+ * 输出: 0
+ * 解释: 3! = 6, 尾数中没有零。
+ * <p>
+ * 示例 2:
+ * 输入: 5
+ * 输出: 1
+ * 解释: 5! = 120, 尾数中有 1 个零.
+ * 说明: 你算法的时间复杂度应为 O(log n)。
+ * <p>
+ * 本质：统计 5 的个数
+ *
+ * @author hc
+ */
+public class Demo172 {
+
+    public int trailingZeroes(int n) {
+        return n == 0 ? 0 : n / 5 + trailingZeroes(n / 5);
+    }
+}
