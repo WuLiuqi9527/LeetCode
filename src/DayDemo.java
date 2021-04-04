4943f44b7f8f942da760fa85a194f0dcbd06013e
10471 <1047106689@qq.com>
2021-04-04 10:14:59 +0800

    2021/4/4

--- src/DayDemo.java
+++ src/DayDemo.java
@@ -3,4 +3,25 @@
  */
 public class DayDemo {
 
+    public int trap(int[] height) {
+        int sum = 0;
+        for (int h : height) {
+            sum += h;
+        }
+
+        int volume = 0;
+        int level = 1;
+        int left = 0, right = height.length - 1;
+        while (left <= right) {
+            while (left <= right && height[left] < level) {
+                ++left;
+            }
+            while (left <= right && height[right] < level) {
+                --right;
+            }
+            volume += right - left + 1;
+            ++level;
+        }
+        return volume - sum;
+    }
 }
