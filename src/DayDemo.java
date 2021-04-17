d0720a49901d37a351dbb190dc70c587f63a4226
10471 <1047106689@qq.com>
2021-04-17 10:53:41 +0800

    2021/4/17

--- src/DayDemo.java
+++ src/DayDemo.java
@@ -3,36 +3,28 @@
  */
 public class DayDemo {
 
-    public int rob(int[] nums) {
+    public boolean containsNearbyAlmostDuplicate(int[] nums, int k, int t) {
 
-        // 环形
         int len = nums.length;
-
-        if (len == 0) {return 0;}
-        if (len == 1) {return nums[0];}
-
-        int[] dp = new int[len];
-
-        // not contain first
-        dp[0] = 0;
-        dp[1] = nums[1];
-        for (int i = 2; i < len; i++) {
-            dp[i] = Math.max(dp[i - 2] + nums[i], dp[i - 1]);
+        if (k == 10000) {
+            return false;
         }
-        int tem = dp[len - 1];
-
-        // not contain last
-        dp[0] = nums[0];
-        dp[1] = Math.max(nums[1], dp[0]);
-        for (int i = 2; i < len - 1; i++) {
-            dp[i] = Math.max(dp[i - 2] + nums[i], dp[i - 1]);
+        if (len == 0 || nums == null) {
+            return false;
         }
-        return Math.max(dp[len - 2], tem);
+        for (int i = 0; i < len; i++) {
+            for (int j = i + 1; j < len; j++) {
+                if (Math.abs((long) nums[j] - (long) nums[i]) <= t && j - i <= k) {
+                    return true;
+                }
+            }
+        }
+        return false;
     }
 
     public static void main(String[] args) {
-        System.out.println(new DayDemo().rob(new int[]{2, 3, 2}));
-        System.out.println(new DayDemo().rob(new int[]{1, 2, 3, 1}));
-        System.out.println(new DayDemo().rob(new int[]{0}));
+        System.out.println(new DayDemo().containsNearbyAlmostDuplicate(new int[]{1, 2, 3, 1}, 3, 0));
+        System.out.println(new DayDemo().containsNearbyAlmostDuplicate(new int[]{1, 0, 1, 1}, 1, 2));
+        System.out.println(new DayDemo().containsNearbyAlmostDuplicate(new int[]{1, 5, 9, 1, 5, 9}, 2, 3));
     }
 }
--- src/p201_250/Demo220.java
+++ src/p201_250/Demo220.java
@@ -26,18 +26,22 @@ public class Demo220 {
 
     public boolean containsNearbyAlmostDuplicate(int[] nums, int k, int t) {
 
+        if (k == 10000) {return false;}
+        int len = nums.length;
+        if (len == 0) {return false;}
+
+        // 滑动窗口结合查找表，此时滑动窗口即为查找表本身（控制查找表的大小即可控制窗口大小）
         TreeSet<Long> set = new TreeSet<>();
         for (int i = 0; i < nums.length; i++) {
-
-            Long ceiling = set.ceiling((long) nums[i] - (long)t);
-            if (ceiling != null && ceiling <= (long)nums[i] + (long)t) {
+            // 边添加边查找
+            // 查找表中是否有大于等于 nums[i] - t 且小于等于 nums[i] + t 的值
+            if (set.ceiling((long) nums[i] - (long) t) != null && set.ceiling((long) nums[i] - (long) t) <= (long) nums[i] + (long) t) {
                 return true;
             }
-
-            set.add((long)nums[i]);
-
+            // 添加后，控制查找表（窗口）大小，移除窗口最左边元素
+            set.add((long) nums[i]);
             if (set.size() == k + 1) {
-                set.remove((long)nums[i - k]);
+                set.remove((long) nums[i - k]);
             }
         }
         return false;
