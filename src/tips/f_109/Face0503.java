ffe7a38bbe932918be83336f936de136b2c3a49a
10471 <1047106689@qq.com>
2021-06-07 16:51:30 +0800

    2021/6/7

---
+++ src/tips/f_109/Face0503.java
@@ -0,0 +1,36 @@
+package tips.f_109;
+
+/**
+ * 给定一个32位整数 num，你可以将一个数位从0变为1。
+ * 请编写一个程序，找出你能够获得的最长的一串1的长度。
+ * <p>示例 1：
+ * 输入: num = 1775(110111011112)
+ * 输出: 8
+ * <p>示例 2：
+ * 输入: num = 7(01112)
+ * 输出: 4
+ *
+ * @author hc
+ */
+public class Face0503 {
+
+    public int reverseBits(int num) {
+        int maxLen = 0, preLen = 0, curLen = 0, bits = 32;
+        while (bits-- > 0) {
+            if ((num & 1) == 0) {
+                curLen -= preLen;
+                preLen = curLen + 1;
+            }
+
+            ++curLen;
+            maxLen = Math.max(maxLen, curLen);
+            num >>= 1;
+        }
+        return maxLen;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new Face0503().reverseBits(1775));
+        System.out.println(new Face0503().reverseBits(7));
+    }
+}
---
+++ src/tips/f_109/Face1601.java
@@ -0,0 +1,28 @@
+package tips.f_109;
+
+/**
+ * 编写一个函数，不用临时变量，直接交换 numbers = [a, b]中 a 与 b 的值。
+ * <p>示例：
+ * 输入: numbers = [1,2]
+ * 输出: [2,1]
+ * <p>提示：
+ * numbers.length == 2
+ * -2147483647 <= numbers[i] <= 2147483647
+ *
+ * @author hc
+ */
+public class Face1601 {
+
+    public int[] swapNumbers(int[] numbers) {
+        return new int[]{numbers[1], numbers[0]};
+    }
+
+    public int[] swapNumbers2(int[] numbers) {
+        // a ^ b ^ b = a;
+        // a ^ b ^ a = b;
+        numbers[0] = numbers[0] ^ numbers[1];
+        numbers[1] = numbers[0] ^ numbers[1];
+        numbers[0] = numbers[0] ^ numbers[1];
+        return numbers;
+    }
+}
