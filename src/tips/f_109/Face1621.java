3d3c07f42d0e9597f44ed00fb85adfa139ad360a
10471 <1047106689@qq.com>
2021-06-07 10:05:55 +0800

    2021/6/7

---
+++ src/tips/f_109/Face1621.java
@@ -0,0 +1,55 @@
+package tips.f_109;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * 给定两个整数数组，请交换一对数值（每个数组中取一个数值），使得两个数组所有元素的和相等。
+ * 返回一个数组，第一个元素是第一个数组中要交换的元素，第二个元素是第二个数组中要交换的元素。
+ * 若有多个答案，返回任意一个均可。若无满足条件的数值，返回空数组。
+ * <p>示例:
+ * 输入: array1 = [4, 1, 2, 1, 1, 2], array2 = [3, 6, 3, 3]
+ * 输出: [1, 3]
+ * <p>示例:
+ * 输入: array1 = [1, 2, 3], array2 = [4, 5, 6]
+ * 输出: []
+ * <p>提示：
+ * 1 <= array1.length, array2.length <= 100000
+ *
+ * @author hc
+ */
+public class Face1621 {
+
+    public int[] findSwapValues(int[] array1, int[] array2) {
+
+        int sum1 = 0, sum2 = 0;
+        for (int arr : array1) {
+            sum1 += arr;
+        }
+        for (int arr : array2) {
+            sum2 += arr;
+        }
+
+        // target = a - b = (sumA - sumB) / 2
+        if (((sum1 - sum2) & 1) == 1) {
+            return new int[0];
+        }
+
+        int target = (sum1 - sum2) >> 1;
+        Set<Integer> set = new HashSet<>();
+        for (int arr1 : array1) {
+            set.add(arr1 - target);
+        }
+        for (int arr2 : array2) {
+            if (set.contains(arr2)) {
+                return new int[]{arr2 + target, arr2};
+            }
+        }
+        return new int[0];
+    }
+
+    public static void main(String[] args) {
+        new Face1621().findSwapValues(new int[]{4, 1, 2, 1, 1, 2}, new int[]{3, 6, 3, 3});
+        new Face1621().findSwapValues(new int[]{1, 2, 3}, new int[]{4, 5, 6});
+    }
+}
--- src/tips/p_1000/p451_500/Demo494.java
+++ src/tips/p_1000/p451_500/Demo494.java
@@ -20,35 +20,58 @@ package tips.p_1000.p451_500;
  */
 public class Demo494 {
 
-    public int findTargetSumWays(int[] nums, int S) {
+    /**
+     * 动态规划
+     */
+    public int findTargetSumWays(int[] nums, int target) {
 
         /**
-         * 2 * sum(P) = S + sum(nums)
-         * 问题转化为能否在 nums 中找到和为 (S + sum(nums)) >>> 1 的子集
+         * sum(P) - sum(N) = target
+         * sum(P) + sum(N) + sum(P) - sum(N) = target + sum(P) + sum(N)
+         * 2 * sum(P) = target + sum(nums)
+         * 问题转化为能否在 nums 中找到一个子集 P，使得 sum(P) = (target + sum(nums)) >>> 1
          */
         int sum = 0;
         for (int num : nums) {
             sum += num;
         }
 
-        return (sum < S) || ((S + sum) & 1) != 0 ? 0 : findTarget(nums, (S + sum) >>> 1);
-    }
+        if (sum < target || ((sum + target) & 1) != 0) {
+            return 0;
+        }
 
-    private int findTarget(int[] nums, int s) {
-
-        /**
-         *  dp[i]代表 从nums中取数相加和为 i 时有多少种取法
-         */
-        int[] dp = new int[s+1];
+        // dp[i]代表 从nums中取数相加和为 i 时有多少种取法
+        int p = (sum + target) >> 1;
+        int[] dp = new int[p + 1];
         dp[0] = 1;
-
-        for(int num:nums){
-            for (int i = s; i >= num; i--) {
-                dp[i] = dp[i] + dp[i-num];
+        for (int num : nums) {
+            for (int i = p; i >= num; i--) {
+                dp[i] += dp[i - num];
             }
         }
 
-        return dp[s];
+        return dp[p];
+    }
+
+    /**
+     * 回溯
+     */
+    private int count = 0;
+
+    public int findTargetSumWays2(int[] nums, int target) {
+        back(nums, target, 0, 0);
+        return count;
+    }
+
+    private void back(int[] nums, int target, int index, int sum) {
+        if (index == nums.length) {
+            if (target == sum) {
+                ++count;
+            }
+        } else {
+            back(nums, target, index + 1, sum + nums[index]);
+            back(nums, target, index + 1, sum - nums[index]);
+        }
     }
 
     public static void main(String[] args) {
