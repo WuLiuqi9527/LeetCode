e619ea0a2673fc48d89cad2fb0321be94e047fea
10471 <1047106689@qq.com>
2021-07-28 14:48:46 +0800

    2021/7/28

---
+++ src/tips/p_1000/p651_700/Demo671.java
@@ -0,0 +1,67 @@
+package tips.p_1000.p651_700;
+
+/**
+ * 给定一个非空特殊的二叉树，每个节点都是正数，并且每个节点的子节点数量只能为 2 或 0。如果一个节点有两个子节点的话，那么该节点的值等于两个子节点中较小的一个。
+ * 更正式地说，root.val = min(root.left.val, root.right.val) 总成立。
+ * 给出这样的一个二叉树，你需要输出所有节点中的第二小的值。如果第二小的值不存在的话，输出 -1 。
+ * <p>示例 1：
+ * 输入：root = [2,2,5,null,null,5,7]
+ * 输出：5
+ * 解释：最小的值是 2 ，第二小的值是 5 。
+ * <p>示例 2：
+ * 输入：root = [2,2,2]
+ * 输出：-1
+ * 解释：最小的值是 2, 但是不存在第二小的值。
+ * <p>提示：
+ * 树中节点数目在范围 [1, 25] 内
+ * 1 <= Node.val <= 2^31 - 1
+ * 对于树中每个节点 root.val == min(root.left.val, root.right.val)
+ *
+ * @author hc
+ */
+public class Demo671 {
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    private int res = -1;
+
+    public int findSecondMinimumValue(TreeNode root) {
+        dfs(root, root.val);
+        return res;
+    }
+
+    private void dfs(TreeNode root, int rootVal) {
+        if (root == null) {
+            return;
+        }
+        // root.val = min(root.left.val, root.right.val)
+        // rootVal 一定是树的最小值 -> 找到除rootVal之外的最小值
+        if (root.val != rootVal) {
+            if (res == -1) {
+                res = root.val;
+            } else {
+                res = Math.min(res, root.val);
+            }
+            return;
+        }
+        dfs(root.left, rootVal);
+        dfs(root.right, rootVal);
+    }
+}
---
+++ src/tips/p_1000/p851_900/Demo863.java
@@ -0,0 +1,122 @@
+package tips.p_1000.p851_900;
+
+import java.util.*;
+
+/**
+ * 给定一个二叉树（具有根结点 root）， 一个目标结点 target ，和一个整数值 K 。
+ * 返回到目标结点 target 距离为 K 的所有结点的值的列表。 答案可以以任何顺序返回。
+ * <p>示例 1：
+ * 输入：root = [3,5,1,6,2,0,8,null,null,7,4], target = 5, K = 2
+ * 输出：[7,4,1]
+ * 解释：
+ * 所求结点为与目标结点（值为 5）距离为 2 的结点，
+ * 值分别为 7，4，以及 1
+ * <p>注意，输入的 "root" 和 "target" 实际上是树上的结点。
+ * 上面的输入仅仅是对这些对象进行了序列化描述。
+ * <p>提示：
+ * 给定的树是非空的。
+ * 树上的每个结点都具有唯一的值 0 <= node.val <= 500 。
+ * 目标结点 target 是树上的结点。
+ * 0 <= K <= 1000.
+ * <p>
+ *
+ * @author hc
+ */
+public class Demo863 {
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    // 用map记录每个节点的父节点
+    private Map<TreeNode, TreeNode> parents = new HashMap<>();
+
+    private Set<TreeNode> used = new HashSet<>();
+
+    private TreeNode targetNode;
+
+    // 找到目标节点后以目标节点为开始位置向三个方向蔓延
+    public List<Integer> distanceK(TreeNode root, TreeNode target, int K) {
+        find(root, null, target);
+        List<Integer> res = new LinkedList<>();
+        dfs(targetNode, res, K);
+        return res;
+    }
+
+    private void find(TreeNode root, TreeNode parent, TreeNode target) {
+        if (null == root) {
+            return;
+        }
+        if (root.val == target.val) {
+            targetNode = root;
+        }
+        parents.put(root, parent);
+        find(root.left, root, target);
+        find(root.right, root, target);
+    }
+
+    private void dfs(TreeNode root, List<Integer> collector, int distance) {
+        if (root != null && !used.contains(root)) {
+            // 标记为已访问
+            used.add(root);
+            if (distance <= 0) {
+                collector.add(root.val);
+                return;
+            }
+            dfs(root.left, collector, distance - 1);
+            dfs(root.right, collector, distance - 1);
+            dfs(parents.get(root), collector, distance - 1);
+        }
+    }
+
+
+    private List<Integer> result;
+
+    public List<Integer> distanceK2(TreeNode root, TreeNode target, int k) {
+        result = new ArrayList<>();
+        getFormCurr(root, target.val, k);
+        return result;
+    }
+
+    private int getFormCurr(TreeNode root, int target, int k) {
+        if (root == null) return -1;
+        if (root.val == target) {
+            readDown(root, k);
+            return k - 1;
+        }
+        int le = getFormCurr(root.left, target, k);
+        if (le >= 0) {
+            if (le == 0) {
+                result.add(root.val);
+            } else {
+                readDown(root.right, le - 1);
+                return le - 1;
+            }
+        }
+        int ri = getFormCurr(root.right, target, k);
+        if (ri >= 0) {
+            if (ri == 0) {
+                result.add(root.val);
+            } else {
+                readDown(root.left, ri - 1);
+                return ri - 1;
+            }
+        }
+        return -1;
+    }
+
+    private void readDown(TreeNode root, int offset) {
+        if (root == null || offset < 0) return;
+        if (offset == 0) {
+            result.add(root.val);
+        }
+        readDown(root.left, offset - 1);
+        readDown(root.right, offset - 1);
+    }
+}
