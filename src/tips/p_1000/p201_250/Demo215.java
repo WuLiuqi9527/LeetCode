671ee11075ac26839ff299f0fcbe19bad9805c86
10471 <1047106689@qq.com>
2021-08-18 10:30:55 +0800

    2021/8/18

--- src/tips/p_1000/p201_250/Demo215.java
+++ src/tips/p_1000/p201_250/Demo215.java
@@ -1,6 +1,10 @@
 package tips.p_1000.p201_250;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
+import java.util.PriorityQueue;
+import java.util.concurrent.CopyOnWriteArrayList;
 
 /**
  * 在未排序的数组中找到第 k 个最大的元素。请注意，你需要找的是数组排序后的第 k 个最大的元素，而不是第 k 个不同的元素。
@@ -22,7 +26,21 @@ public class Demo215 {
         return nums[nums.length - k];
     }
 
-    public static void main(String[] args) {
+    public int findKthLargest2(int[] nums, int k) {
+        PriorityQueue<Integer> queue = new PriorityQueue<>();
+        for (int num : nums) {
+            if (queue.size() < k) {
+                queue.add(num);
+            }else if (queue.peek() < num){
+                queue.remove();
+                queue.add(num);
+            }
+        }
+        return queue.peek();
+    }
+
+        public static void main(String[] args) {
         System.out.println(new Demo215().findKthLargest(new int[]{3, 2, 1, 5, 6, 4}, 2));
+        System.out.println(new Demo215().findKthLargest2(new int[]{3, 2, 1, 5, 6, 4}, 2));
     }
 }
---
+++ src/tips/p_1000/p551_600/Demo552.java
@@ -0,0 +1,105 @@
+package tips.p_1000.p551_600;
+
+/**
+ * 可以用字符串表示一个学生的出勤记录，其中的每个字符用来标记当天的出勤情况（缺勤、迟到、到场）。记录中只含下面三种字符：
+ * 'A'：Absent，缺勤
+ * 'L'：Late，迟到
+ * 'P'：Present，到场
+ * 如果学生能够 同时 满足下面两个条件，则可以获得出勤奖励：
+ * 按 总出勤 计，学生缺勤（'A'）严格 少于两天。
+ * 学生 不会 存在 连续 3 天或 连续 3 天以上的迟到（'L'）记录。
+ * 给你一个整数 n ，表示出勤记录的长度（次数）。请你返回记录长度为 n 时，可能获得出勤奖励的记录情况 数量 。
+ * 答案可能很大，所以返回对 10^9 + 7 取余 的结果。
+ * <p>示例 1：
+ * 输入：n = 2
+ * 输出：8
+ * 解释：
+ * 有 8 种长度为 2 的记录将被视为可奖励：
+ * "PP" , "AP", "PA", "LP", "PL", "AL", "LA", "LL"
+ * 只有"AA"不会被视为可奖励，因为缺勤次数为 2 次（需要少于 2 次）。
+ * <p>示例 2：
+ * 输入：n = 1
+ * 输出：3
+ * <p>示例 3：
+ * 输入：n = 10101
+ * 输出：183236316
+ * <p>提示：
+ * 1 <= n <= 10^5
+ *
+ * @author hc
+ */
+public class Demo552 {
+
+    final int MOD = 10000_00007;
+
+    public int checkRecord(int n) {
+        // 前 i 天有 j 个 ‘A’ 且结尾有连续 k 个 ‘L’ 的可奖励的出勤记录的数量
+        int[][][] dp = new int[n + 1][2][3];
+        dp[0][0][0] = 1;
+        for (int i = 1; i <= n; i++) {
+            // 以 P 结尾
+            for (int j = 0; j <= 1; j++) {
+                for (int k = 0; k <= 2; k++) {
+                    dp[i][j][0] = (dp[i][j][0] + dp[i - 1][j][k]) % MOD;
+                }
+            }
+            // 以 A 结尾
+            for (int k = 0; k <= 2; k++) {
+                dp[i][1][0] = (dp[i][1][0] + dp[i - 1][0][k]) % MOD;
+            }
+            // 以 L 结尾
+            for (int j = 0; j <= 1; j++) {
+                for (int k = 1; k <= 2; k++) {
+                    dp[i][j][k] = (dp[i][j][k] + dp[i - 1][j][k - 1]) % MOD;
+                }
+            }
+        }
+        int res = 0;
+        for (int j = 0; j <= 1; j++) {
+            for (int k = 0; k <= 2; k++) {
+                res = (res + dp[n][j][k]) % MOD;
+            }
+        }
+        return res;
+    }
+
+    public int checkRecord2(int n) {
+        // dp[i][][] 只会从 dp[i−1][][] 转移得到。
+        // 因此可以将 dp 中的总天数的维度省略
+        int[][] dp = new int[2][3];
+        dp[0][0] = 1;
+        for (int i = 1; i <= n; i++) {
+            int[][] dpNew = new int[2][3];
+            // 以 P 结尾的
+            for (int j = 0; j <= 1; j++) {
+                for (int k = 0; k <= 2; k++) {
+                    dpNew[j][0] = (dpNew[j][0] + dp[j][k]) % MOD;
+                }
+            }
+            // 以 A 结尾
+            for (int k = 0; k <= 2; k++) {
+                dpNew[1][0] = (dpNew[1][0] + dp[0][k]) % MOD;
+            }
+            // 以 L 结尾
+            for (int j = 0; j <= 1; j++) {
+                for (int k = 1; k <= 2; k++) {
+                    dpNew[j][k] = (dpNew[j][k] + dp[j][k - 1]) % MOD;
+                }
+            }
+            dp = dpNew;
+        }
+        int sum = 0;
+        for (int j = 0; j <= 1; j++) {
+            for (int k = 0; k <= 2; k++) {
+                sum = (sum + dp[j][k]) % MOD;
+            }
+        }
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new Demo552().checkRecord(1));
+        System.out.println(new Demo552().checkRecord(2));
+        System.out.println(new Demo552().checkRecord(10101));
+    }
+}
