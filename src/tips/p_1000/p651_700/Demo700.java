4bedc3bb0439afb66f3c32f403ae011ad1fb9c7e
10471 <1047106689@qq.com>
2021-11-26 19:48:18 +0800

    2021/11/26

---
+++ src/tips/p_1000/p651_700/Demo700.java
@@ -0,0 +1,57 @@
+package tips.p_1000.p651_700;
+
+/**
+ * 给定二叉搜索树（BST）的根节点和一个值。 你需要在BST中找到节点值等于给定值的节点。
+ * 返回以该节点为根的子树。 如果节点不存在，则返回 NULL。
+ * <p>例如，
+ * 给定二叉搜索树:
+ * 4
+ * / \
+ * 2   7
+ * / \
+ * 1   3
+ * 和值: 2
+ * 你应该返回如下子树:
+ * 2
+ * / \
+ * 1   3
+ * 在上述示例中，如果要找的值是 5，但因为没有节点值为 5，我们应该返回 NULL。
+ *
+ * @author hc
+ */
+
+public class Demo700 {
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public TreeNode searchBST(TreeNode root, int val) {
+        if (root == null || root.val == val) {
+            return root;
+        }
+        return root.val < val ? searchBST(root.right, val) : searchBST(root.left, val);
+    }
+
+    public TreeNode searchBST2(TreeNode root, int val) {
+        while (root != null && root.val != val) {
+            root = root.val < val ? root.right : root.left;
+        }
+        return root;
+    }
+}
