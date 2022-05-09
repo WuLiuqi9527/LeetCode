c6765842cb7892208319a148199cecc7e6896954
10471 <1047106689@qq.com>
2022-05-09 11:53:17 +0800

    2022/5/9

---
+++ src/tips/p_1000/p901_950/Demo942.java
@@ -0,0 +1,35 @@
+package tips.p_1000.p901_950;
+
+/**
+ * 由范围 [0,n] 内所有整数组成的 n + 1 个整数的排列序列可以表示为长度为 n 的字符串 s ，其中:
+ * 如果 perm[i] < perm[i + 1] ，那么 s[i] == 'I'
+ * 如果 perm[i] > perm[i + 1] ，那么 s[i] == 'D'
+ * 给定一个字符串 s ，重构排列 perm 并返回它。如果有多个有效排列perm，则返回其中 任何一个 。
+ * <p>示例 1：
+ * 输入：s = "IDID"
+ * 输出：[0,4,1,3,2]
+ * <p>示例 2：
+ * 输入：s = "III"
+ * 输出：[0,1,2,3]
+ * <p>示例 3：
+ * 输入：s = "DDI"
+ * 输出：[3,2,0,1]
+ * <p>提示：
+ * 1 <= s.length <= 105
+ * s 只包含字符 "I" 或 "D"
+ *
+ * @author hc
+ */
+public class Demo942 {
+
+    public int[] diStringMatch(String s) {
+        int n = s.length(), l = 0, r = n, idx = 0;
+        int[] ans = new int[n + 1];
+        char[] chars = s.toCharArray();
+        for (int i = 0; i < n; i++) {
+            ans[idx++] = chars[i] == 'I' ? l++ : r--;
+        }
+        ans[idx] = l;
+        return ans;
+    }
+}
