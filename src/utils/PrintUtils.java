13e789e8f4e35b38221ad8eae6e2f9ddb2dcc29a
Huang <1047106689@qq.com>
2022-10-05 23:34:47 +0800

    2022/10/5

--- src/utils/PrintUtils.java
+++ src/utils/PrintUtils.java
@@ -3,6 +3,7 @@ package utils;
 import common.ListNode;
 
 import java.util.Arrays;
+import java.util.List;
 
 public class PrintUtils {
 
@@ -11,7 +12,7 @@ public class PrintUtils {
         System.out.println(Arrays.stream(arr).boxed().toList());
     }
 
-    // 打印链表
+    // 打印 链表
     public static void printListNode(ListNode root) {
         if (root == null) {
             return;
@@ -23,5 +24,12 @@ public class PrintUtils {
             root = root.next;
         }
         System.out.println(sb);
+    }
+
+    // 打印 list<String>
+    public static void printListString(List<String> list) {
+        for (String s : list) {
+            System.out.println(s);
+        }
     }
 }
