16138e526e08d873d0871f2643a0b74e3b868dcf
Huang <1047106689@qq.com>
2023-01-04 00:03:44 +0800

    2023/1/3

--- src/tips/p_others/Demo2042.java
+++ src/tips/p_others/Demo2042.java
@@ -1,2 +1,21 @@
-package tips.p_others;public class Demo242 {
+package tips.p_others;
+
+public class Demo2042 {
+
+    public boolean areNumbersAscending(String s) {
+        String[] tokens = s.split(" ");
+        int start = -1;
+        for (int i = 0; i < tokens.length; i++) {
+            if (!Character.isDigit(tokens[i].charAt(0))) {
+                continue;
+            }
+            int tem = Integer.parseInt(tokens[i]);
+            if (start >= tem) {
+                return false;
+            }
+            start = tem;
+        }
+
+        return true;
+    }
 }
