cee4d3bed57ea35c38b2062bcb9bb5794430241b
10471 <1047106689@qq.com>
2023-05-02 10:10:03 +0800

    2023/5/2

---
+++ src/tips/p_1000/p951_1000/Demo970.java
@@ -0,0 +1,21 @@
+package tips.p_1000.p951_1000;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class Demo970 {
+
+    public List<Integer> powerfulIntegers(int x, int y, int bound) {
+        Set<Integer> ans = new HashSet<>();
+        for (int a = 1; a < bound; a *= x) {
+            for (int b = 1; a + b <= bound; b *= y) {
+                ans.add(a + b);
+                if (y == 1) break;
+            }
+            if (x == 1) break;
+        }
+        return ans.stream().toList();
+    }
+}
