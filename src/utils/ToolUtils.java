c36505e7f6945ff354bcee463ad849a7a86ddee6
Huang <1047106689@qq.com>
2024-04-22 23:44:57 +0800

    feat:新增toolUtils

---
+++ src/utils/ToolUtils.java
@@ -0,0 +1,12 @@
+package utils;
+
+public class ToolUtils {
+
+    public static int pow(int a, int b){
+        if (b <= 0) {
+            return 1;
+        }
+        return a * pow(a, b - 1);
+    }
+
+}
